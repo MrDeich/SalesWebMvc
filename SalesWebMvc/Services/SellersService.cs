b8c259ecf9070f7fd604a71e542b5d64545223e6
Tiago Portilho <tiagoportilho52@gmail.com>
2022-08-16 18:34:44 -0300

    Upadate seller and custom service exception

--- SalesWebMvc/Services/SellersService.cs
+++ SalesWebMvc/Services/SellersService.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exeptions;
 
 namespace SalesWebMvc.Services
 {
@@ -39,5 +41,25 @@ namespace SalesWebMvc.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+
+
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
